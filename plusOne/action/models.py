8a2f2d1b36b99e35665eda5b6f295f94fce59946
v15hv4 <vishva2912@gmail.com>
2020-05-24 02:28:28 +0530

    Switch to local time

--- plusOne/action/models.py
+++ plusOne/action/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from datetime import datetime
-# Create your models here.
+from django.utils import timezone
+
+
 class Trip(models.Model):
     source = models.CharField(max_length = 100)
     destination = models.CharField(max_length = 100)
-    journey_date = models.DateField(default = datetime.now)
-    journey_time = models.TimeField(default = datetime.now)
+    journey_date = models.DateField(default = timezone.localtime(timezone.now()))
+    journey_time = models.TimeField(default = timezone.localtime(timezone.now()))
     is_active = models.BooleanField(default = False)
     owner = models.CharField(max_length = 100, null = True, blank = True)
