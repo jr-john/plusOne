24b08eaf8860f29242f5abfa874015d0280c154e
jr-john <jerrin2001@gmail.com>
2020-05-23 21:25:57 +0530

    Revert "Merge pull request #1 from loki-777/master"
    This reverts commit e7827ec621c55158f014e071879b8e3825ececf2, reversing
    changes made to 0c4eac9d6aeb2c7290a496faf5084622f05eb642.

--- plusOne/home/urls.py
+++ plusOne/home/urls.py
@@ -1,11 +1,5 @@
 from django.urls import path
 
-from . import views
-
 urlpatterns = [
-    #path("register", views.register, name="register"),
-    #path("login", views.login, name="login"),
-    #path("logout", views.logout, name="logout")
-    path("NewTrip", views.NewTrip, name="NewTrip"),
-    path("", views.home, name="home")
-    ]
+    
+]
--- plusOne/home/views.py
+++ plusOne/home/views.py
@@ -1,8 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-def NewTrip(request):
-    return render(request, 'entry.html')
-
-def home(request):
-    return render(request, "home.html")
