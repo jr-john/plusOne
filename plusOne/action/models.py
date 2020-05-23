a8692aacf013e258ba74d9c392266fc3d2083eb1
jr-john <jerrin2001@gmail.com>
2020-05-23 22:13:13 +0530

    Create Trip Model

--- plusOne/action/models.py
+++ plusOne/action/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class Trip(models.Model):
+    source = models.CharField(max_length = 100)
+    destination = models.CharField(max_length = 100)
+    journey_datetime = models.DateTimeField(default = datetime.now)
+    is_active = models.BooleanField(default = False)
+    owner = models.CharField(max_length = 100)  
