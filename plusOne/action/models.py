7cdbfc9d5ef344cfd1eacc151e2d7c6d79d39e91
arvindh75 <iiith@arvindharun.com>
2020-05-24 17:45:03 +0530

    Removed Seconds from default time

--- plusOne/action/models.py
+++ plusOne/action/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
 
+def def_time():
+    return timezone.now().strftime("%H:%M")
 
 class Trip(models.Model):
     source = models.CharField(max_length = 100)
     destination = models.CharField(max_length = 100)
     journey_date = models.DateField(default = timezone.now)
-    journey_time = models.TimeField(default = timezone.now)
+    journey_time = models.TimeField(default = def_time)
     is_active = models.BooleanField(default = False)
     owner = models.CharField(max_length = 100, null = True, blank = True)
