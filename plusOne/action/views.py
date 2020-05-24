22727dd4eeef3d6c360b4fae4c94aa464abfe7c4
Lokesh Gautham B M <loki-777@github.com>
2020-05-24 17:11:52 +0530

    Fixing +1 2.0

--- plusOne/action/views.py
+++ plusOne/action/views.py
@@ -4,7 +4,7 @@ from .models import Trip
 from .forms import TripForm
 import datetime
 import json
-
+from django.contrib.auth.models import User
 
 # @login_required
 def home(request, *args, **kwargs):
@@ -79,3 +79,15 @@ def add(request, *args, **kwargs):
     search_query.is_active = True
     search_query.save()
     return redirect("/")
+
+
+def tripdetails(request):
+    id = request.GET['id']
+    temp = Trip.objects.query(id = id)
+    var=temp.owner
+    curr_owner = User.objects.query(username=var)
+    context={
+        'temp' : temp,
+        'curr_owner' : curr_owner
+    }
+    return render(request, 'tripdetails.html', context)
