563bfafbae6dc4fb9e9d2d9d4198bbe5c4d24c12
arvindh75 <iiith@arvindharun.com>
2020-05-24 18:54:46 +0530

    Dropdown without Frontend

--- plusOne/action/models.py
+++ plusOne/action/models.py
@@ -4,9 +4,19 @@ from django.utils import timezone
 def def_time():
     return timezone.now().strftime("%H:%M")
 
+TAG_LIST = [
+    ['airport', 'Rajiv Gandhi Intenational Airport'],
+    ['railway-secunderabad', 'Secunderabad Railway Station'],
+    ['railway-lingam', 'Lingampally Railway Station'],
+    ['railway-begumpet', 'Begumpet Railway Station'],
+    ['railway-nampally', 'Hyderabad Deccan Railway Station (Nampally)'],
+    ['bus-mg', 'Mahatma Gandhi Bus Station'],
+    ['college','IIIT Hyderabad Campus']
+]
+
 class Trip(models.Model):
-    source = models.CharField(max_length = 100)
-    destination = models.CharField(max_length = 100)
+    source = models.CharField(max_length = 100, choices = TAG_LIST, default = 'other')
+    destination = models.CharField(max_length = 100, choices = TAG_LIST, default = 'other')
     journey_date = models.DateField(default = timezone.now)
     journey_time = models.TimeField(default = def_time)
     is_active = models.BooleanField(default = False)
