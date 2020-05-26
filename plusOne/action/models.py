aeec0ca714f0a10c6b062e9becba1f84ea69b434
Lokesh Gautham B M <lokeshgautham13@gmail.com>
2020-05-26 12:54:52 +0530

    Fixed default time

--- plusOne/action/models.py
+++ plusOne/action/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.utils import timezone
+from datetime import datetime, timedelta, date
 
 def def_time():
     return timezone.now().strftime("%H:%M")
 
+def def_journey_time():
+    dt = timezone.now() + timedelta(minutes=10)
+    return dt.time()
+
+def def_journey_time_max():
+    dt = timezone.now() + timedelta(minutes=20)
+    return dt.time()
 TAG_LIST = [
     ['airport', 'Rajiv Gandhi International Airport'],
     ['railway-secunderabad', 'Secunderabad Railway Station'],
@@ -18,8 +26,8 @@ class Trip(models.Model):
     source = models.CharField(max_length = 100, choices = TAG_LIST, default = 'college')
     destination = models.CharField(max_length = 100, choices = TAG_LIST, default = 'airport')
     journey_date = models.DateField(default = timezone.now)
-    journey_time = models.TimeField(default = def_time)
+    journey_time = models.TimeField(default = def_journey_time)
     is_active = models.BooleanField(default = False)
     owner = models.CharField(max_length = 100, null = True, blank = True)
     minima = models.TimeField(default = def_time)
-    maxima = models.TimeField(default = def_time)
+    maxima = models.TimeField(default = def_journey_time_max)
